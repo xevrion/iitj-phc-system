1a68afee13dbc47217f91ed4a9d64e1c241b339d
Chaitanya Singh <chaitanyacoder11@gmail.com>
2026-04-08 01:22:09 +0530

    feat(frontend): setup core infrastructure, routing, and utils

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import LoginPage from "./features/auth/pages/LoginPage";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <div className='bg-black h-screen flex flex-col items-center justify-center gap-6'>
-        <h1 className='text-blue-400 text-3xl'>Welcome to the IITJ PHC System!</h1>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<div className="flex h-screen items-center justify-center text-red-500 text-xl font-bold">404: Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
